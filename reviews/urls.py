7946909a9e3e41f8d4cfc6841451d363de6c9df7
ganavi12 <ganavi.k@actcorp.local>
2021-05-05 02:14:15 +0530

    class based view

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [  
-    path("", views.review),
-    path("thankyou",views.thank_you)
+    # path("", views.review),
+    path("",views.ReviewView.as_view()),
+    path("thankyou",views.thank_you) 
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -2,15 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
+from django.views import View
 
 # Create your views here.
+
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, "reviews/review.html", {"form": form})
+    
+    def post(self, request):
+        form = ReviewForm(request.POST) 
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thankyou")
+        form = ReviewForm()
+        return render(request, "reviews/review.html", {"form": form})
 
 def review(request):
     if request.method == "POST":
         form = ReviewForm(request.POST) 
-        if form.is_valid(): 
-            review = Review(username=form.cleaned_data['username'],review_text =form.cleaned_data['review_text'],rating =form.cleaned_data['rating'] )
-            review.save()
+        if form.is_valid():
+            form.save()
+            # review = Review(username=form.cleaned_data['username'],review_text =form.cleaned_data['review_text'],rating =form.cleaned_data['rating'] )
+            # review.save()
             # print(form.cleaned_data)
             # entered_username = request.POST['username']
         # if entered_username == "" or not entered_username.isalpha(): 
