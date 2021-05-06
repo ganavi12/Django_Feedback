2dc428285d325006a432b8b99775edc818a5324d
ganavi12 <ganavi.k@actcorp.local>
2021-05-06 23:14:31 +0530

    upload image

--- feedback/settings.py
+++ feedback/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 # MEDIA_URL = os.path.join(BASE_DIR, 'user-media')
 
 
@@ -122,5 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = MEDIA_ROOT 
+MEDIA_URL = "/user-media/"
+
+
 
 
--- profiles/models.py
+++ profiles/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class UserProfile(models.Model):
     image = models.ImageField(upload_to="images")
+    # image = models.FileField(upload_to="images")
